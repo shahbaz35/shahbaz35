7cfadcf83fca2833635cdfc27222e6867b92ea8b
shahbaz35 <71803899+shahbaz35@users.noreply.github.com>
2022-05-17 23:59:45 +0100

    app fully working just need to test

--- connecting_to_database/.idea/app3.py
+++ connecting_to_database/.idea/app3.py
@@ -1,7 +1,13 @@
 from tkinter import *
 import os
 import mysql.connector
-
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import mysql.connector
+import chromedriver_autoinstaller as chromedriver
+chromedriver.install()
 #########################
 
 root = Tk()
@@ -21,7 +27,33 @@ message_LabelA = Label(root, text="Error: User Email Address is Empty !!!", bg =
 message_LabelB = Label(root, text="Error: User Password is Empty !!!", bg = "light blue")
 noUserFoundLabel = Label(root, text="No User Found With These Details", bg= "Light Blue")
 userFoundLabel = Label(root, text="User Found", bg= "Light Blue")
-PCNFailLabel = Label(root, text="Error: PCN Number is Empty ", padx = 50, pady = 10, bg= "Light Blue")
+pcnFailLabel = Label(root, text="Error: PCN Number is Empty ", padx = 50, pady = 10, bg="Light Blue")
+regFailLabel = Label(root, text="Error: Car Registration empty ", padx = 50, pady = 10, bg="Light Blue")
+automationLabel = Label(root, text="The Automation will begin shortly... ", padx = 50, pady = 10, bg= "Light Blue")
+
+# import unittest
+# from appium import webdriver
+
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import WebDriverWait
+startURL = 'https://my.redbridge.gov.uk/parkingpcn/'
+currentURL = startURL
+auto_refNo = ''
+auto_numberPlate = ''
+auto_nameOnCard = ''
+auto_cardNumber = ''
+auto_expiryMonth = ''
+auto_expiryYear = ''
+auto_cvvNumber = ''
+auto_name = ''
+auto_emailAddress = ''
+error = ''
+currentError = ''
+
+
+
+
+
 def connect():
     try:
         mydb = mysql.connector.connect(host="localhost",
@@ -66,8 +98,8 @@ def getCardDetails(x, y):
             noUserFoundLabel.grid(row= 8,column=0)
         else:
             # print(accountID)
-            FinalemailAddress = email
-            print(FinalemailAddress)
+            auto_emailAddress = email
+            print(auto_emailAddress)
             FinalPassword = password
             print(FinalPassword)
             noUserFoundLabel.grid_remove()
@@ -86,16 +118,16 @@ def getCardDetails(x, y):
         try :
             cardID = cardDetails[0]
             print("Card ID: "), print(cardID)
-            digits16 = cardDetails[1]
-            print("16 Digits: " + digits16)
-            expiry1 = cardDetails[2]
-            print("Expiry 1: " + expiry1)
-            expiry2 = cardDetails[3]
-            print("Expiry 2: " + expiry2)
-            cvv = cardDetails[4]
-            print("CVV: " + cvv)
-            CHName = cardDetails[5]
-            print("CardHolder Name: "+CHName)
+            auto_cardNumber = cardDetails[1]
+            print("16 Digits: " + auto_cardNumber)
+            auto_expiryMonth = cardDetails[2]
+            print("Expiry 1: " + auto_expiryMonth)
+            auto_expiryYear = cardDetails[3]
+            print("Expiry 2: " + auto_expiryYear)
+            auto_cvvNumber = cardDetails[4]
+            print("CVV: " + auto_cvvNumber)
+            auto_nameOnCard = cardDetails[5]
+            print("CardHolder Name: "+auto_nameOnCard)
         except IndexError:
             pass
 
@@ -135,25 +167,148 @@ def page2():
     message_LabelC.grid_remove()
     userFoundLabel.grid_remove()
     page2LabelA = Label(root, text=" Pay For Parking Charge Notice ", padx = 50, pady = 10, bg= "Light Blue").grid(row=1, column=0)
-    PCNLabel = Label(root, text=" Enter Full PCN Number Below: ", padx = 50, pady = 10, bg= "Light Blue").grid(row=2, column=0)
+    pcnLabel = Label(root, text=" Enter Full PCN Number Below: ", padx = 50, pady = 10, bg= "Light Blue").grid(row=2, column=0)
 
-    PCNTextBox = Entry(root, width=50, borderwidth= 4)
-    PCNTextBox.grid(row= 3,column=0, padx= 50, pady= 10)
-    submitButton = Button(root, text=" Login ",width= 15,  command=lambda : submission(PCNTextBox.get()), fg = "black", bg = "light pink")
-    submitButton.grid(row= 4,column= 0, padx= 50, pady= 15)
+    pcnTextBox = Entry(root, width=50, borderwidth= 4)
+    pcnTextBox.grid(row= 3,column=0, padx= 50, pady= 10)
+
+    regLabel = Label(root, text=" Enter Car Registration Without Spaces: ", padx = 50, pady = 10, bg= "Light Blue").grid(row=4, column=0)
+    regTextBox = Entry(root, width=50, borderwidth= 4)
+    regTextBox.grid(row= 5,column=0, padx= 50, pady= 10)
+
+    submitButton = Button(root, text=" Submit ",width= 15,  command= lambda : submission(pcnTextBox.get(), regTextBox.get()), fg = "black", bg = "light pink")
+    submitButton.grid(row= 6,column= 0, padx= 50, pady= 15)
 
 
 
-def submission(x):
-    print("Submission")
-    print("x == "+x)
-    PCN_Tiket = x
-    if PCN_Tiket == '':
-        PCNFailLabel.grid(row=5, column=0)
+def submission(PCN,reg):
+    print("PCN: "+PCN+"    , REG: "+reg)
+    auto_refNo = PCN
+    auto_numberPlate = reg
+    if auto_refNo == '':
+        pcnFailLabel.grid(row=7, column=0)
     else:
-        PCNFailLabel.grid_remove()
-        # this is where all details from database will be gathered and the automation will begin
+        pcnFailLabel.grid_remove()
+    if auto_numberPlate == '':
+        regFailLabel.grid(row=8, column=0)
+    else:
+        regFailLabel.grid_remove()
+    if auto_refNo != '' and auto_numberPlate != '':
+        pcnFailLabel.grid_remove()
+        regFailLabel.grid_remove()
 
+        automation(auto_refNo, auto_numberPlate)
+
+        # this is where all details from database will be gathered and the automation will begin
+def automation(x,y):
+    print("Automation stage for redbridge website")
+    automationLabel.grid(row= 8, column= 0)
+    global driver
+    driver = webdriver.Chrome()
+    driver.get(startURL)
+
+    inputPCNBox = driver.find_element(by=By.XPATH, value='//*[@id="Pcn"]')
+    # the box below is where the reference number will go automatically
+    inputPCNBox.send_keys(x)
+    inputRegNoBox = driver.find_element(by=By.XPATH, value='//*[@id="Vrn"]')
+    inputRegNoBox.send_keys(y)
+    searchButton1 = driver.find_element(by=By.XPATH,
+                                        value='//*[@id="formsubmitpcnsearch"]/div/div/div/div/div/div/div[3]/input[2]')
+    driver.implicitly_wait(1)
+    searchButton1.click()
+    currentURL = driver.current_url
+    print()
+    if startURL == currentURL:
+        error = "PCN number or vehicle Registration not entered"
+        errorChecker(error)
+    else:
+        """
+        try:
+            driver.find_element(by=By.XPATH, value='//*[@id="Pcn"]').is_displayed()
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
+        """
+
+        try:
+            if driver.find_element(by=By.XPATH, value='//*[@id="Pcn"]').is_displayed():
+                print("Error, Still On Page 1: ")
+                currentError = driver.find_element(by=By.XPATH, value='//*[@id="MainSection"]/div[3]').text
+                errorChecker(currentError)
+                print()
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
+        print("on to page 2, No Errors on page 1")
+        print()
+    try:
+        price = driver.find_element(by=By.XPATH, value='//*[@id="one"]/p/strong').text
+        print(price)
+    except selenium.common.exceptions.NoSuchElementException:
+        pass
+    infoContraventionDate = driver.find_element(by=By.XPATH, value='//*[@id="one"]/div/div[1]/div/div/div[1]/ul/li[3]').text
+    print(infoContraventionDate)
+    infoContravention = driver.find_element(by=By.XPATH, value='//*[@id="one"]/div/div[1]/div/div/div[1]/ul/li[4]').text
+    print(infoContravention)
+    infoStreet = driver.find_element(by=By.XPATH, value='//*[@id="one"]/div/div[1]/div/div/div[1]/ul/li[5]').text
+    print(infoStreet)
+    infoLocation = driver.find_element(by=By.XPATH, value='//*[@id="one"]/div/div[1]/div/div/div[1]/ul/li[6]').text
+    print(infoLocation)
+    try:
+        status = driver.find_element(by=By.XPATH, value='//*[@id="one"]/div/div/div/div/div[1]/ul/li[7]').text
+        if status == 'Status: Fully Paid':
+            errorChecker(status)
+    except selenium.common.exceptions.NoSuchElementException:
+        pass
+    driver.implicitly_wait(2)
+    payPCNButton = driver.find_element(by=By.XPATH, value='//*[@id="one"]/div/div[2]/div/div/div/div/a')
+    payPCNButton.click()
+    payPCNOnlineButton = driver.find_element(by=By.XPATH, value='//*[@id="pcn-pay-fines"]/p[2]/a')
+    payPCNOnlineButton.click()
+    # After clicking on the Payment button there is also a long wait
+    checkoutButton = driver.find_element(by=By.XPATH, value='//*[@id="divShoppingBasketView"]/div[4]/div/div/a[1]')
+    checkoutButton.click()
+    nameOnCardBox = driver.find_element(by=By.XPATH, value='//*[@id="CardDetailsModel_NameOnCard"]')
+    nameOnCardBox.send_keys(auto_nameOnCard)
+    cardNumberBox = driver.find_element(by=By.XPATH, value='//*[@id="CardDetailsModel_CardNumber"]')
+    cardNumberBox.send_keys(auto_cardNumber)
+    expiryMonthBox = driver.find_element(by=By.XPATH, value='//*[@id="CardDetailsModel_SelectedExpiryMonth"]')
+    expiryMonthBox.send_keys(auto_expiryMonth)
+    expiryYearBox = driver.find_element(by=By.XPATH, value='//*[@id="CardDetailsModel_SelectedExpiryYear"]')
+    expiryYearBox.send_keys(auto_expiryYear)
+    cvcNumberBox = driver.find_element(by=By.XPATH, value='//*[@id="CardDetailsModel_CSC"]')
+    cvcNumberBox.send_keys(auto_cvvNumber)
+    driver.implicitly_wait(1)
+
+    checkoutSummaryButton = driver.find_element(by=By.XPATH, value='//*[@id="CheckOutSummary"]')
+    checkoutSummaryButton.click()
+    driver.implicitly_wait(2)
+    try:
+        if driver.find_element(by=By.XPATH, value='//*[@id="error-summary-titleitem-0"]').is_displayed():
+            print("Payment Error, Still On Payment Page: ")
+            currentError = driver.find_element(by=By.XPATH, value='//*[@id="error-summary-titleitem-0"]').text
+            errorChecker(currentError)
+            print()
+    except selenium.common.exceptions.NoSuchElementException:
+        pass
+
+
+def errorChecker(x):
+    error = x
+    if error == '':
+        print()
+        print("No Errors")
+        print()
+    elif error != '':
+        print()
+        print(error)
+        print("Error Has Been Found Exit Code Initiated")
+        try :
+            driver.close()
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
+        finalMessageLabel = Label(root, text= error, bg = "Light Blue")
+        finalMessageLabel.grid(row = 10,column = 0)
+        # driver.quit()
+        #raise SystemExit(0)
 
 
 emailLabel = Label(root, text="Enter User Email: ", bg = "light blue")
